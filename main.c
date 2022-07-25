42c7f292d7cc31d9122003a5d91f5f3d03e50bfc
TangPPP <2581462872@qq.com>
2022-07-25 21:52:44 +0800

    b站学习c语言鹏哥  循环和分支  输入三次密码

---
+++ main.c
@@ -0,0 +1,29 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <stdlib.h>
+int main()
+{
+	int i = 0;
+	char password[20] = {0};
+	printf("«Î ‰»Î√‹¬Î£∫");	
+	for (i = 0; i < 3; i++)
+	{
+		scanf("%s", password);
+		if (strcmp(password, "511725") == 0)
+		{
+			system("cls");
+			printf("√‹¬Î’˝»∑\n");
+			break;
+		}
+		system("cls");
+		printf("√‹¬Î¥ÌŒÛ£¨«Î÷ÿ–¬ ‰»Î£∫");
+	}
+	if (i == 3)
+	{
+		system("cls");
+		printf(" ‰»Î3¥Œ√‹¬Î¥ÌŒÛ£¨«ÎÕÀ≥ˆ\n");
+	}
+	return 0;
+}
